253215fcaa562f44ac4843a45442f9f0de4b2138
roel1289 <roel1289@colorado.edu>
2020-01-28 20:02:48 -0700

    This is an updated version of Lab 1 final problem

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,1 +1,2 @@
-cut -f 2-4 -d , PredPreyData.csv | tail PredPreyData.csv >> Lab01finalproblem.csv
+cut -f 2-4 -d, PredPreyData.csv | head -n 1 > PredPreyData.csv
+cut -f 2-4 -d, PredPreyData.csv | tail -n 10 >> PredPreyData.csv
