0e7cd5332dbcc7de4852856b72d219beb0619de4
roel1289 <roel1289@colorado.edu>
2020-02-05 10:48:05 -0700

    I am submitting an updated version of lab01finalproblem

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,2 +1,4 @@
-cut -f 2-4 -d, PredPreyData.csv | head -n 1 > PredPreyData.csv
-cut -f 2-4 -d, PredPreyData.csv | tail -n 10 >> PredPreyData.csv
+cut -f 2-4 -d, PredPreyData.csv | head -n 1 > lab01finalProblem.csv    #This command takes the output of columns 2-4 and adds the first row of PredPreyData.csv
+cut -f 2-4 -d, PredPreyData.csv | tail -n 10 >> lab01finalProblem.csv  #This command takes the output of columns 2-4 and adds the last 10 rows of PredPreyData.csv
+
+#both of these commands are made into a new file named lab01finalProblem.csv
