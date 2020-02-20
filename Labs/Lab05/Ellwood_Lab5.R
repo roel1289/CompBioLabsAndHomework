3dc52383deef41d7804602f8f2efb77e9859266e
roel1289 <roel1289@colorado.edu>
2020-02-19 22:32:46 -0700

    the first half of lab05

--- Labs/Lab05/Ellwood_Lab5.R
+++ Labs/Lab05/Ellwood_Lab5.R
@@ -32,4 +32,24 @@ myVector
 sum(myVector <= 100 & myVector >= 50)
 
 #Step 2e: Making new datavector named "FiftyToOneHundred"  
-FiftyToOneHundred <- 
+FiftyToOneHundred <- which(myVector > 50 & myVector < 100)
+str(FiftyToOneHundred)
+
+#Step 2f: saving "FiftyToOneHundred" as a .csv
+write.csv(x = FiftyToOneHundred, file = "FiftyToOneHundred.csv")
+
+##Step 3: Here I will be importing CO2 data 
+CO2Data <- read.csv("CO2_data_cut_paste.csv")
+str(CO2Data) #looking at the structure of the data
+View(CO2Data)
+
+#Step 3a: Determining the first year which data on "Gas" were non-zero
+which(CO2Data$Gas != 0)
+#based of the "which()" function, it appears the 135th year had the first non-zero value
+#Looking at the data, the 135th year is 1885
+
+#Step 3b: the years where "Total" emission was between 200 and 300 million tons of carbon
+which(CO2Data$Total > 200 & CO2Data$Total < 300)
+#The years span from the 129th year to the 137th year (1879 to 1887)
+
+###Part 4: Loops + Conditionals + Biology
