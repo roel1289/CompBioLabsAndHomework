71d1053d5b7b06c757d61303af30b26d0eebefa8
roel1289 <roel1289@colorado.edu>
2020-02-27 14:59:50 -0700

    I am adding the beginning of my Lab07 script

---
+++ Labs/Lab07/Ellwood_Lab7.R
@@ -0,0 +1,8 @@
+###Problem 1: Writing a function that calculates the area of a triangle ((0.5 * base) * Height)
+myTriangleAreaFunc <- function(base,height){  #the function variables are base and height
+  myTriAreaEq <- (0.5 * base * height)          #MyTriArea is the equation to calculate triangle area
+  return(myTriAreaEq)
+}
+myTriangleAreaFunc(10, 9)
+
+###Problem 2: 
