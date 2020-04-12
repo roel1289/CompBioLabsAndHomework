9c4370c8186fd4b988216e76f78940358d5ae35a
roel1289 <roel1289@colorado.edu>
2020-04-11 21:20:36 -0600

    My progress on Assgt. 8

--- Assignments/Assignment08/Ellwood_Assgt08.R
+++ Assignments/Assignment08/Ellwood_Assgt08.R
@@ -1,22 +1,33 @@
 ###Assignment 8
 
 #importing the data
-FishLengthDiet <- read.csv("~/EBIO4420/CompBioLabsAndHomework/Assignments/Assignment08/FishLengthDiet.csv")
+FishLengthDiet <- read.csv("~/EBIO4420/CompBioLabsAndHomework/Assignments/Assignment08/FishLengthDiet.csv", stringsAsFactors = F)
 
 #Question: Does using different gear to catch the fish create a bias for the certain length of fish caught?
 
 #looking into only columns "Gear", "SPECIES" and "TL_MM"
 gearFishLength <- FishLengthDiet[ ,2:4] #now we can focus on the gear used to find each species and the fish length
-gearFishLength
+str(gearFishLength)
+
+#check for any missing data entries(will be shown as "NA") and delete those rows 
+#noNAGearFishLength1 <- gearFishLength[-which(is.na(gearFishLength$TL_MM))] 
+
+#install following packages to delete "NA" rows
+install.packages("tidyverse") #need tidyverse for following command
+library(dplyr)
+library(tidyr)
+noNAGearFishLength <- drop_na(gearFishLength)
+
+#check to see if worked
+which(is.na(noNAGearFishLength2)) #it worked!
 
 #filter into unique gear entries
 #find average length for each gear option
+meanLengthByGear <- summarize(
+  group_by(noNAGearFishLength,
+      GEAR),
+  meanLength = mean(TL_MM))
 
-
-#use tidyverse package
-install.packages("tidyverse")
-
-#example using tidyverse pipes
-library("ggplot2")
-myAnswer <- fishDietStudyDetailed %>% tail(DIET_ITEM)
-library(ggplot2)
+View(meanLengthBySpecies)
+#so far, I have found the avg. length of each fish species using each type of gear method
+plot(meanLengthByGear)
