606312fbf32c1fca276a8af96ae8642d5c0cd179
roel1289 <roel1289@colorado.edu>
2020-04-21 21:24:29 -0600

    Here is progress on my r

--- Assignments/Assignment09/Ellwood_Assgt09.R
+++ Assignments/Assignment09/Ellwood_Assgt09.R
@@ -2,8 +2,8 @@
 
 #in order to run my code, you must have these packages:
 install.packages("tidyverse") 
-library(dplyr)
-library(tidyr)
+require(dplyr)
+require(tidyr)
 
 
 #importing the data
@@ -19,28 +19,28 @@ FishLengthDiet <- read.csv("~/EBIO4420/CompBioLabsAndHomework/Assignments/Assign
 gearFishLength <- FishLengthDiet[ ,c(2,4)] #now we can focus on the gear used and the fish length
 str(gearFishLength)
 
-#check for any missing data entries(will be shown as "NA") and delete those rows 
-#noNAGearFishLength1 <- gearFishLength[-which(is.na(gearFishLength$TL_MM))] 
-
 #Now, I will remove any "NA"'s from the dataframe. 
 noNAGearFishLength <- drop_na(gearFishLength) #uses tidyr
 
 #check to see if worked by searching for "NA"
 which(is.na(noNAGearFishLength)) #it worked, no "NA's" were found!
+head(noNAGearFishLength)
 
-#####
-#Changing from long format to wide format:
-#####
-#gearFishLengthWide <- spread(noNAGearFishLength, GEAR, TL_MM)
-#gearFishLengthWide
+#For some reason, there are 4 types of gear entries:  "HOOP"  , "SEINE" , "DIPNET", and ""
+#We must delete the "" entry from the GEAR column
 
-#####
 
+#barplot using "aggregate" and by making a table 
+#########################
+aggOutput <- aggregate(noNAGearFishLength$TL_MM, by = list(noNAGearFishLength$GEAR), FUN = sum)
+aggOutput
+barplot(height = aggOutput$x, names.arg = aggOutput$Group.1)
+#########################
 
 barplot(height = noNAGearFishLength$TL_MM, names = noNAGearFishLength$GEAR)
 
 bPlotTable <- table(noNAGearFishLength$TL_MM, noNAGearFishLength$GEAR)
-barplot(bPlot)
+barplot(bPlotTable)
 
 ################
 
